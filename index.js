84201f2c8b9ca5ced7ab81fce051c1510d89b1fc
MrLinna <linnanantti@outlook.com>
2023-01-09 15:44:15 +0200

    exercise 3.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-let persons = [  
+let contacts = [  
     {    
         id: 1,    
         name: "Arto Hellas",    
@@ -23,18 +23,30 @@ let persons = [
         name: "Mary Poppendick",    
         number: "39-23-6423122"  
     }
-    ]
+]
 
 app.get('/api/persons', (req, res) => {
-    res.json(notes)
+    res.json(contacts)
 })
+
 app.get('/info', (req, res) => {
     res.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
+        <p>Phonebook has info for ${contacts.length} people</p>
         <p>${Date()}</p>
     `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const contact = contacts.find(contact => contact.id === id)
+    if (contact){
+        res.json(contact)
+    }
+    else{
+        res.status(404).end()
+    }
+
+})
 
 const PORT = 3001
     app.listen(PORT, () => {
