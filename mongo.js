a8c19e89811d30273354aa01be26e03041c53c1d
MrLinna <78093433+MrLinna@users.noreply.github.com>
2023-01-24 17:04:34 +0200

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const e = require('express')
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.ibxxa0c.mongodb.net/phoneBookDb?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+// if the password is the only parameter
+if (process.argv.length == 3){
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+          console.log(contact.name, contact.number)
+        })
+        mongoose.connection.close()
+    })
+}
+// if name and number are given as parameters
+else{
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const contact = new Contact({
+        name: name,
+        number: number,
+    })  
+
+    contact.save().then(result => {
+        console.log(`added ${name, number} to phonebook`)
+        mongoose.connection.close()
+
+    })
+}
+
+
